8c1f27c13a33e06fdaf9acaf5a37558032975e4a
carlosalbt <carlosalbt@live.com>
2020-11-19 08:51:54 -0300

    Adicionando entidades, enums e repositorios

---
+++ src/main/java/com/carlos/pontoeletronico/enums/PerfilEnum.java
@@ -0,0 +1,7 @@
+package com.carlos.pontoeletronico.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+
+}
---
+++ src/main/java/com/carlos/pontoeletronico/repositories/EmpresaRepository.java
@@ -0,0 +1,12 @@
+package com.carlos.pontoeletronico.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.carlos.pontoeletronico.entities.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+
+}
